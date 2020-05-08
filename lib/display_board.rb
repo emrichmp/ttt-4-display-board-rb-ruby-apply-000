2a10f7329e7c4c765e418c922c15545c52d51cd8
Emrich-Michael Perrier <emrichmp@gmail.com>
2020-05-08 18:58:54 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,5 +3,8 @@
 board = ["O", "X", " ", " ", "X", " ", "X", "O", " "]
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]}"
-  puts"
+  puts"-----------"
+  puts " #{board[0]} | #{board[1]} | #{board[2]}"
+  puts"-----------"
+  puts " #{board[0]} | #{board[1]} | #{board[2]}"
 end
